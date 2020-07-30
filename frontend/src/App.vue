8e8c28c46c5925bff0f7e3c17a6874b94ceeaa4b
JINSUB <neyo0311@naver.com>
2020-07-31 03:27:10 +0900

    에디터에서 이미지 메타데이터 추출 완료

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,58 +1,18 @@
 <template>
   <div id="app">
-    <Header :isHeader="isHeader"/>
-    <router-view/>
-    <Footer/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>  
-<script> 
-import './assets/css/style.scss' 
-import Header from './components/common/Header.vue'
-import Footer from './components/common/Footer.vue'
-import constants from './lib/constants' 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: { 
-    Header,
-    Footer
+  components: {
+    HelloWorld
   },
-  created() {
-      let url = this.$route.name;
-
-      this.checkUrl(url);
-  },
-  watch: {
-      $route (to){
-
-          this.checkUrl(to.name);
-      }
-  },
-  methods : {
-      checkUrl(url) { 
-
-          let array = [
-              constants.URL_TYPE.USER.LOGIN,
-              constants.URL_TYPE.USER.JOIN,
-          ];
-
-          let isHeader = true;
-          array.map(path => {
-              if (url === path)
-                  isHeader = false;
-          })
-          this.isHeader = isHeader;
-
-      },
-  },
-  data: function () {
-        return {
-            isHeader: true,
-            constants
-        } 
-    }
 }
 </script>
 
@@ -60,7 +20,7 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale; 
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
